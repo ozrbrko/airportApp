e33bbb113aa1e3f7fe04e714b26cbe85db184f04
burak.ozer <oozerburak@gmail.com>
2022-10-11 09:41:07 +0300

    design updated

--- lib/view/airports_list_page.dart
+++ lib/view/airports_list_page.dart
@@ -55,7 +55,7 @@ class _AirportsListPageState extends State<AirportsListPage> {
           )
         ],
         backgroundColor: Color(0xff224459),
-        elevation: 20,
+        elevation: 5,
         shadowColor: Color(0xffD97D48),
       ),
       body: WillPopScope(
@@ -84,169 +84,179 @@ class _AirportsListPageState extends State<AirportsListPage> {
 
         case PageState.SUCCESS:
           return SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  child: Column(
-                    // mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: FrameSize.screenHeight! / 100,
-                            left: FrameSize.screenWidth! / 10,
-                            right: FrameSize.screenWidth! / 10),
-                        child: SizedBox(
-                          height: FrameSize.screenHeight! / 12.91428571428572,
-                          child: Padding(
-                            padding: EdgeInsets.all(
-                                FrameSize.screenHeight! / 96.85714285714286),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Color(0xffC9D6F5), //0xffD97D48
-                                borderRadius: BorderRadius.only(
-                                    topRight: Radius.circular(
-                                        FrameSize.screenHeight! /
-                                            25.82857142857143),
-                                    bottomRight: Radius.circular(
-                                        FrameSize.screenHeight! /
-                                            25.82857142857143),
-                                    topLeft: Radius.circular(
-                                        FrameSize.screenHeight! /
-                                            25.82857142857143),
-                                    bottomLeft: Radius.circular(
-                                        FrameSize.screenHeight! /
-                                            25.82857142857143)),
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.only(
-                                    left: FrameSize.screenHeight! /
-                                        43.04761904761905,
-                                    bottom: FrameSize.screenHeight! /
-                                        258.2857142857143),
-                                child: Stack(children: [
-                                  TextField(
-                                    controller: tfSearch,
-                                    decoration: InputDecoration(
-                                        hintText: "Ara",
-                                        border: InputBorder.none),
-                                  ),
-                                  Positioned(
-                                      left: FrameSize.screenWidth! / 1.7,
-                                      top: FrameSize.screenHeight! /
-                                          154.9714285714286,
-                                      child: GestureDetector(
-                                        onTap: () {
-                                          print(tfSearch.text);
-                                        },
-                                        child: Container(
-                                            height: FrameSize.screenHeight! /
-                                                22.13877551020408,
-                                            width: FrameSize.screenHeight! /
-                                                22.13877551020408,
-                                            decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(
-                                                      FrameSize.screenHeight! /
-                                                          25.82857142857143)),
-                                              color: Colors.white,
-                                            ),
-                                            child: Icon(Icons.search)),
-                                      ))
-                                ]),
+            child: Container(
+              color: Colors.grey[100],
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.grey[100],
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: FrameSize.screenHeight! / 100,
+                              left: FrameSize.screenWidth! / 10,
+                              right: FrameSize.screenWidth! / 10),
+                          child: SizedBox(
+                            height: FrameSize.screenHeight! / 12.91428571428572,
+                            child: Padding(
+                              padding: EdgeInsets.all(
+                                  FrameSize.screenHeight! / 96.85714285714286),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color(0xffC9D6F5), //0xffD97D48
+                                  borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(
+                                          FrameSize.screenHeight! /
+                                              25.82857142857143),
+                                      bottomRight: Radius.circular(
+                                          FrameSize.screenHeight! /
+                                              25.82857142857143),
+                                      topLeft: Radius.circular(
+                                          FrameSize.screenHeight! /
+                                              25.82857142857143),
+                                      bottomLeft: Radius.circular(
+                                          FrameSize.screenHeight! /
+                                              25.82857142857143)),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      left: FrameSize.screenHeight! /
+                                          43.04761904761905,
+                                      bottom: FrameSize.screenHeight! /
+                                          258.2857142857143),
+                                  child: Stack(children: [
+                                    TextField(
+                                      controller: tfSearch,
+                                      decoration: InputDecoration(
+                                          hintText: "Ara",
+                                          border: InputBorder.none),
+                                    ),
+                                    Positioned(
+                                        left: FrameSize.screenWidth! / 1.7,
+                                        top: FrameSize.screenHeight! /
+                                            154.9714285714286,
+                                        child: GestureDetector(
+                                          onTap: () {
+                                            print(tfSearch.text);
+                                          },
+                                          child: Container(
+                                              height: FrameSize.screenHeight! /
+                                                  22.13877551020408,
+                                              width: FrameSize.screenHeight! /
+                                                  22.13877551020408,
+                                              decoration: BoxDecoration(
+                                                borderRadius: BorderRadius.all(
+                                                    Radius.circular(
+                                                        FrameSize.screenHeight! /
+                                                            25.82857142857143)),
+                                                color: Colors.white,
+                                              ),
+                                              child: Icon(Icons.search)),
+                                        ))
+                                  ]),
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Container(
-                    color: Colors.blue,
-                    height: FrameSize.screenHeight!/1.25,
-                    child: ListView.builder(
-                        itemCount: airportsStore!.airportsResultList!.length,
-                        itemBuilder: (context, index) {
-                          AirportsResult? item =
-                              airportsStore!.airportsResultList![index];
-                          return Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Container(
-                                  width: FrameSize.screenWidth,
-                                  height: FrameSize.screenHeight!/9,
-                                  color: Colors.white,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(2.0),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-                                        Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Container(
+                      color: Colors.grey[100],
+                      height: FrameSize.screenHeight!/1.25,
+                      child: ListView.builder(
+                          itemCount: airportsStore!.airportsResultList!.length,
+                          itemBuilder: (context, index) {
+                            AirportsResult? item =
+                                airportsStore!.airportsResultList![index];
+                            return Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(12),
+                                      color: Colors.white,
 
 
-                                          children: [
-                                            Text(
-                                              "${item!.shortName.toString()} ${item!.countryCode.toString()}",
-                                              style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                                            ),
+                                    ),
+
+                                    width: FrameSize.screenWidth,
+                                    height: FrameSize.screenHeight!/7,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(15.0),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
 
 
-                                            Row(
-                                              children: [
-                                                Column(
-                                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "${item!.shortName!.toString()} ${item.countryCode!.toString()}",
+                                                style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                                              ),
 
-                                                  children: [
-                                                    Padding(
-                                                      padding: const EdgeInsets.only(top: 8.0),
-                                                      child: Text(
-                                                          "Icao: ${item!.icao.toString()} "
+                                              Row(
+                                                children: [
+                                                  Column(
+                                                    crossAxisAlignment: CrossAxisAlignment.start,
+
+                                                    children: [
+                                                      Padding(
+                                                        padding: const EdgeInsets.only(top: 8.0),
+                                                        child: Text(
+                                                            "Icao: ${item.icao.toString()} "
+                                                        ),
                                                       ),
-                                                    ),
-                                                    Padding(
-                                                      padding: const EdgeInsets.only(top: 8.0),
-                                                      child: Text(
-                                                          "Iata: ${item!.iata.toString()} "                                                      ),
-                                                    ),
-                                                  ],
-                                                ),
-                                                Column(
-                                                  children: [
-
-                                                    Padding(
-                                                      padding: const EdgeInsets.only(top: 8.0,left: 15.0),
-                                                      child: Text(
-                                                          "Lat: ${item!.location!.lat.toString()} "
+                                                      Padding(
+                                                        padding: const EdgeInsets.only(top: 8.0),
+                                                        child: Text(
+                                                            "Iata: ${item.iata.toString()} "                                                      ),
                                                       ),
-                                                    ),
-                                                    Padding(
-                                                      padding: const EdgeInsets.only(top: 8.0,left: 23),
-                                                      child: Text(
-                                                          "Lon: ${item!.location!.lon.toString()} "                                                      ),
-                                                    ),
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
+                                                    ],
+                                                  ),
+                                                  Column(
+                                                    children: [
 
-                                          ],
-                                        ),
+                                                      Padding(
+                                                        padding: const EdgeInsets.only(top: 8.0,left: 15.0),
+                                                        child: Text(
+                                                            "Lat: ${item.location!.lat.toString()} "
+                                                        ),
+                                                      ),
+                                                      Padding(
+                                                        padding: const EdgeInsets.only(top: 8.0,left: 23),
+                                                        child: Text(
+                                                            "Lon: ${item.location!.lon.toString()} "                                                      ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ],
+                                              ),
 
-                                      ],
+                                            ],
+                                          ),
+
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                              )
-                            ],
-                          );
-                        }),
-                  ),
-                )
-              ],
+                                )
+                              ],
+                            );
+                          }),
+                    ),
+                  )
+                ],
+              ),
             ),
           );
         default:
